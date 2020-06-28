79df15d4ac283927ad7adc97dd67f233b1a00837
abdennour001 <amokranabdennour@gmail.com>
2020-06-29 00:34:27 +0100

    learning

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,19 +1,22 @@
+require("dotenv").config();
+
 export const config = {
-  "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
-    "dialect": "postgres",
-    "aws_region": "us-east-2",
-    "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
-  },
-  "prod": {
-    "username": "",
-    "password": "",
-    "database": "udagram_prod",
-    "host": "",
-    "dialect": "postgres"
-  }
-}
+    dev: {
+        username: process.env.POSTGRES_USERNAME,
+        password: process.env.POSTGRES_PASSWORD,
+        database: process.env.POSTGRES_DATABASE,
+        host: process.env.POSTGRES_HOST,
+        port: process.env.POSTGRES_PORT,
+        dialect: process.env.POSTGRES_DIALECT,
+        aws_region: process.env.POSTGRES_AWS_REGION,
+        aws_profile: process.env.POSTGRES_AWS_PROFILE,
+        aws_media_bucket: process.env.POSTGRES_AWS_MEDIA_BUCKET
+    },
+    prod: {
+        username: "udagramamokrane",
+        password: "Abdou123",
+        database: "udagram_prod",
+        host: "udagramamokrane.cmuksxu17fap.us-east-2.rds.amazonaws.com",
+        dialect: "postgres"
+    }
+};
