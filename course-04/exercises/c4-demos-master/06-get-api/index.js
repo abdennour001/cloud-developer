b9547c61981c1ecbd584e61e13602bbae0bb56f5
abdennour001 <amokranabdennour@gmail.com>
2020-08-17 10:26:48 +0100

    update ✅

--- course-04/exercises/c4-demos-master/06-get-api/index.js
+++ course-04/exercises/c4-demos-master/06-get-api/index.js
@@ -2,23 +2,23 @@
 
 const data = [
   {
-    id: '1',
-    name: 'Dogs',
-    description: 'Only dog images here!'
+    id: "1",
+    name: "Dogs",
+    description: "Only dog images here!"
   },
   {
-    id: '2',
-    name: 'Nature',
-    description: 'What can be a better object for photography',
+    id: "2",
+    name: "Nature",
+    description: "What can be a better object for photography"
   },
   {
-    id: '3',
-    name: 'Cities',
-    description: 'Creative display of urban settings',
+    id: "3",
+    name: "Cities",
+    description: "Creative display of urban settings"
   }
 ]
 
-exports.handler = async (event) => {
+exports.handler = async event => {
   console.log('Processing event: ', event)
 
   return {
