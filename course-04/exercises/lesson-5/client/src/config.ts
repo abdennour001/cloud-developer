d7d52bd29a689855f3084f35e8beb8db59aa9847
abdennour001 <amokranabdennour@gmail.com>
2020-08-30 17:05:00 +0100

    finish up lesson 5

--- course-04/exercises/lesson-5/client/src/config.ts
+++ course-04/exercises/lesson-5/client/src/config.ts
@@ -1,7 +1,7 @@
-export const apiEndpoint = '...'
+export const apiEndpoint = "https://4390c77shj.execute-api.us-east-2.amazonaws.com/prod"
 
 export const authConfig = {
-  domain: '...',
-  clientId: '...',
+  domain: 'abdennour.eu.auth0.com',
+  clientId: 'ezrYOpqUpoeL3dXJ1E8V9pZSRBhwGnL9',
   callbackUrl: 'http://localhost:3000/callback'
 }
