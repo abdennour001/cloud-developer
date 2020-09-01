e8b19f39235a730be4b9d9b609b092a74b8934aa
abdennour001 <amokranabdennour@gmail.com>
2020-09-01 22:36:14 +0100

    finish up front end

--- course-04/project/c4-final-project-starter-code/client/src/config.ts
+++ course-04/project/c4-final-project-starter-code/client/src/config.ts
@@ -1,10 +1,10 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '...'
-export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+const apiId = '8dptt3iddc'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/dev`
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'abdennour.eu.auth0.com', // Auth0 domain
+  clientId: 'ezrYOpqUpoeL3dXJ1E8V9pZSRBhwGnL9', // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
